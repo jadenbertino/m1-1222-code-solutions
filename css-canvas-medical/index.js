4607545ac8a7be924db3cadeb329b000161481cf
Jaden Bertino <jaden@bertinofamily.com>
2022-12-21 02:45:09 +0000

    simplify popup event listeners to hamburgerBtn and xBtn

--- css-canvas-medical/index.js
+++ css-canvas-medical/index.js
@@ -1,13 +1,12 @@
 const popUp = document.querySelector('.mobile-popup-wrapper');
 const hamburgerBtn = document.querySelector('.nav-hamburger-btn');
-const xIcon = document.querySelector('.mobile-x');
-const xBtn = document.querySelector('.fa-times');
+const xBtn = document.querySelector('.nav-x-btn');
 
 function togglePopUp() {
   popUp.classList.toggle('active');
 }
 
-[popUp, hamburgerBtn, xIcon, xBtn].forEach(elem => {
+[hamburgerBtn, xBtn].forEach(elem => {
   elem.addEventListener('click', togglePopUp);
 });
 
