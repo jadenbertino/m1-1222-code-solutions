712fceb4c6042a3ecefb6c53a4fb520aac799cab
Jaden Bertino <jaden@bertinofamily.com>
2022-12-27 21:55:37 -0800

    bookshelf.js console logs

--- javascript-data-model/bookshelf.js
+++ javascript-data-model/bookshelf.js
@@ -15,3 +15,9 @@ var bookshelf = [
     author: 'Phil Sturgeon & Laura Bohill'
   }
 ];
+
+console.log("Name of author of second book:", bookshelf[1].author);
+
+console.log("ISBN of first book:", bookshelf[0].isbn);
+
+console.log("title of third book:", bookshelf[2].title);
