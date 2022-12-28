47880f2b0683847b0006ac107b5f42fb65672f6e
Jaden Bertino <jaden@bertinofamily.com>
2022-12-27 21:52:46 -0800

    create initial data model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,74 @@
+var orderHistory = [
+  {
+    orderDate: "August 4, 2020",
+    address: "JS Masher",
+    totalPrice: 34,
+    id: "114-3941689-8772232",
+    items: [
+      {
+        name: "JavaScript for impatient programmers",
+        vendor: "Rauschmayer, Dr. Axel",
+        quantity: 1,
+        price: 31.55,
+        deliveryDate: "August 8, 2020",
+        returnWindow: "September 7, 2020"
+      }
+    ]
+  },
+  {
+    orderDate: "July 19, 2020",
+    address: "JS Masher",
+    totalPrice: 44.53,
+    id: "113-9984268-1280257",
+    items: [
+      {
+        name: "The Timeless Way of Building",
+        vendor: "Alexander, Christopher",
+        quantity: 1,
+        price: 41.33,
+        deliveryDate: "July 20, 2020",
+        returnWindow: "August 19, 2020"
+      }
+    ]
+  },
+  {
+    orderDate: "July 4, 2020",
+    address: "JS Masher",
+    totalPrice: 17.22,
+    id: "114-2875557-9059409",
+    items: [
+      {
+        name: "Gamecube Controller Adapter", // no way I'm writing the whole name xD
+        vendor: null,
+        quantity: 1,
+        price: 15.98,
+        deliveryDate: "July 7, 2020",
+        returnWindow: "August 5, 2020"
+      }
+    ]
+  },
+  {
+    orderDate: "July 3, 2020",
+    address: "JS Masher",
+    totalPrice: 138.93,
+    id: "113-2883177-2648248",
+    items: [
+      {
+        name: "GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)",
+        vendor: null,
+        quantity: 1,
+        price: 94.95,
+        deliveryDate: "July 5, 2020",
+        returnWindow: "August 4, 2020"
+      },
+      {
+        name: "The Art of Sql",
+        vendor: "Faroult, Stephanie",
+        quantity: 1,
+        price: 33.99,
+        deliveryDate: "July 5, 2020",
+        returnWindow: "August 4, 2020"
+      }
+    ]
+  },
+];
