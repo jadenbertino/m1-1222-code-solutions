2f51edaf957ac38a20da81a6c7611775e5989b63
Jaden Bertino <jaden@bertinofamily.com>
2023-01-06 11:05:31 -0800

    create sum-all.js solution with reduce method

--- javascript-loops-practice/sum-all.js
+++ javascript-loops-practice/sum-all.js
@@ -1,9 +1,10 @@
 /* exported sumAll */
 
 function sumAll(numbers) {
-  let sum = 0;
-  for (let num of numbers) {
-    sum += num
-  }
-  return sum;
+  return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+  // let sum = 0;
+  // for (let num of numbers) {
+  //   sum += num
+  // }
+  // return sum;
 }
