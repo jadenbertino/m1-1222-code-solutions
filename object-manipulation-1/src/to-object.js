778f4a9c2ef5640dc431e8d38fc6d7dd651d0670
Jaden Bertino <jaden@bertinofamily.com>
2023-01-09 20:55:07 -0800

    convert toObject.js to one liner!!!

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,8 +1,5 @@
 /* exported toObject */
 
 function toObject([key, val]) {
-  if (typeof key !== 'string') {return 'invalid'}
-  const obj = {}
-  obj[key] = val
-  return obj
+  return (typeof key !== 'string') ? 'invalid' : { [key]: val }
 }
