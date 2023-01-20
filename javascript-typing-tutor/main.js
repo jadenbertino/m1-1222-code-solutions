82108a56ff5481c64deea904c9dbcc686599856f
Jaden Bertino <jaden@bertinofamily.com>
2023-01-19 22:13:24 -0800

    implement wpm tracker

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,20 +1,26 @@
+/*
+  TODO:
+  ✅ Game start screen
+  Measure time from starting of first typing to finish time and calculate WPM (word per minute)
+  Come up with more set of sentence or words and randomly generate it
+  Leaderboard with name and score
+  Time-attack mode (sentence fades away)
+  Be creative!
+*/
 function newGame() {
   // Game Setup
   $startScreen.classList.add('hidden')
   $gameOverDisplay.classList.remove('game-over-active')
   $newGameBtn.classList.add('hidden');
   $gameOverDisplay.textContent = '';
-  const sentence = "grumpy wizards make toxic brew";
+  const sentence = 'yo' // "grumpy wizards make toxic brew";
   const chars = sentence.split('');
-  const display = chars
-    .map((char, i) => {
-      if (i) {
-        return `<span class="char">${char}</span>`;
-      }
-      return `<span class="char active">${char}</span>`; // first char should have active class
-    })
-    .join('');
+  const display = chars.map((char, i) => {
+    if (i) { return `<span class="char">${char}</span>` }
+    return `<span class="char active">${char}</span>`; // first char should have active class
+  }).join('');
   $sentenceBox.innerHTML = display;
+  const startTime = window.performance.now();
 
   // listen for keypress -> respond to correct & wrong keypress
   let $htmlChar = document.querySelector('.char:not(.correct)');
@@ -43,13 +49,11 @@ function newGame() {
     }
 
     // game over
-    if (
-      gameOver &&
-      $gameOverDisplay.className !== 'game-over game-over-active'
-    ) {
-      $gameOverDisplay.textContent = `Congrats, you win! You're typing accuracy was ${Math.floor(
-        (chars.length / userTypeCount) * 100
-      )}% 🎉`;
+    if (gameOver && !$gameOverDisplay.classList.contains('game-over-active')) {
+      const typingAccuracy = Math.floor(chars.length / userTypeCount * 100)
+      const typingTime = Math.floor(window.performance.now() - startTime) / 1000
+      const wpm = Math.floor((60 / typingTime) * sentence.length)
+      $gameOverDisplay.innerHTML = `Congrats, you win! 🎉<br />You're typing accuracy was ${typingAccuracy}%<br />You typed at ${wpm} words per minute.`;
       $gameOverDisplay.className = 'game-over game-over-active';
       $newGameBtn.textContent = 'Play Again?';
       $newGameBtn.classList.remove('hidden');
